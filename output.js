2d01d514952422b062ddf7cdcec4f5ec2c698ec6
LukeBurns52 <143461719+LukeBurns52@users.noreply.github.com>
2025-03-31 11:02:39 -0400

    Create output.js

---
+++ output.js
@@ -0,0 +1,12 @@
+function showInnerHTML() {
+	document.getElementById("output1").innerHTML = "I am Zero.";
+}
+function useDocumentWrite() {
+	document.write("This replaces the page content.");
+}
+function showConsole() {
+	console.log("I AM IN YOUR WALLS!!!!!!");
+}
+function showAlert() {
+	alert("DODGE THIS");
+}
