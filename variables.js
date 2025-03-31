310119ae6e51d3136a3acb58b4c3c374148c038c
LukeBurns52 <143461719+LukeBurns52@users.noreply.github.com>
2025-03-31 18:18:09 -0400

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+let name = "Zero"
+let age = "Unknown"
+let isStudent= true;
+
+document.getElementById("demo1").innerHTML = "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+var dingus = "Zion"
+let goober = true;
+
+document.getElementById("demo2").innerHTML = "Dingus: " + dingus + "<br>Is he a Goober: " + goober;
